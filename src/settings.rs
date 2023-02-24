9c05a075b3bc74bf27717f7bc951b01d954b0ce6
cq01 <chq_github@outlook.com>
2023-02-24 13:02:42 +0800

    重构logger和settings (#36)
    * 重构logger,settings
    
    * fix: upgrade to clap v4
    
    * 依赖更新
    
    ---------
    
    Co-authored-by: SeaLoong <sealoongw@foxmail.com>

--- src/settings.rs
+++ src/settings.rs
@@ -1,19 +1,42 @@
-use std::cmp::max;
+use chrono::NaiveTime;
+use clap::ArgMatches;
+use config::{Config, FileFormat, Value};
+use log::error;
+use pnet::datalink::MacAddr;
 use std::collections::HashMap;
 use std::net::{IpAddr, SocketAddr};
 use std::path::Path;
 use std::str::FromStr;
+use std::sync::LazyLock;
 
-use chrono::NaiveTime;
-use config::{Config, FileFormat, Value};
-use log::{error, LevelFilter};
-use pnet::datalink::MacAddr;
+fn get_matches() -> ArgMatches {
+    use clap::*;
+    command!(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .args(&[
+            arg!(-c --config <config> "Path to config file.").default_value("config.yml"),
+            arg!(-D --debug "Enable debug mode.").action(ArgAction::SetTrue),
+            arg!(-m --mac <mac> "Ethernet Device MAC address.").required(false),
+            arg!(-i --ip <ip> "IP address of the selected Ethernet Device.").required(false),
+            arg!(-u --username <username> "Username to authorize.").required(false),
+            arg!(-p --password <password> "Password to authorize.").required(false),
+            arg!(-H --host <host> "Host to connect UDP server. Default value is 's.scut.edu.cn'.").required(false),
+            arg!(-N --hostname <hostname> "Default value is current computer host name.").required(false),
+            arg!(-t --time <time> "Time to reconnect automatically after you are not allowed to access Internet. Default value is 7:00.").required(false),
+        ])
+        .get_matches()
+}
+
+static MATCHES: LazyLock<ArgMatches> = LazyLock::new(get_matches);
+pub static DEBUG: LazyLock<bool> = LazyLock::new(|| MATCHES.get_flag("debug"));
+pub static SETTINGS: LazyLock<Settings> = LazyLock::new(|| Settings::parse(&MATCHES));
+
+const DEFAULT_CONFIG_FILE: &str = include_str!("default_config.yml");
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone)]
 pub struct Settings {
-    pub debug: bool,
-    pub noudp: bool,
-    pub path: String,
     pub mac: Option<MacAddr>,
     pub ip: Option<IpAddr>,
     pub username: String,
@@ -26,15 +49,13 @@ pub struct Settings {
     pub heartbeat: Heartbeat,
     pub retry: Retry,
     pub data: Data,
+    #[cfg(feature = "log4rs")]
     pub log: Log,
 }
 
 impl Default for Settings {
     fn default() -> Self {
         Settings {
-            debug: false,
-            noudp: false,
-            path: String::from("config.yml"),
             mac: None,
             ip: None,
             username: String::new(),
@@ -47,7 +68,7 @@ impl Default for Settings {
             heartbeat: Heartbeat::default(),
             retry: Retry::default(),
             data: Data::default(),
-            #[cfg(feature = "enablelog")]
+            #[cfg(feature = "log4rs")]
             log: Log::default(),
         }
     }
@@ -83,21 +104,23 @@ impl Default for Retry {
     }
 }
 
+#[cfg(feature = "log4rs")]
 #[derive(Debug, Hash, PartialEq, Eq, Clone)]
 pub struct Log {
     pub enable_console: bool,
     pub enable_file: bool,
     pub file_directory: String,
-    pub level_filter: LevelFilter,
+    pub level_filter: log::LevelFilter,
 }
 
+#[cfg(feature = "log4rs")]
 impl Default for Log {
     fn default() -> Self {
         Log {
             enable_console: true,
             enable_file: true,
             file_directory: String::from("./logs"),
-            level_filter: LevelFilter::Info,
+            level_filter: log::LevelFilter::Info,
         }
     }
 }
@@ -180,77 +203,56 @@ fn get_map_from_map(map: &HashMap<String, Value>, k: &str) -> Option<HashMap<Str
     map.get(k)?.to_owned().into_table().ok()
 }
 
-fn get_str(matches: &clap::ArgMatches, cfg: &config::Config, k: &str) -> Option<String> {
+fn get_str(matches: &ArgMatches, cfg: &Config, k: &str) -> Option<String> {
     matches
-        .value_of(k)
-        .map(|s| s.to_string())
+        .try_get_one::<String>(k)
+        .ok()
+        .flatten()
+        .cloned()
         .or_else(|| cfg.get_string(k).ok())
         .filter(|s| !s.trim().is_empty())
 }
 
-fn get_u64(matches: &clap::ArgMatches, cfg: &config::Config, k: &str) -> Option<u64> {
+fn get_u64(matches: &ArgMatches, cfg: &Config, k: &str) -> Option<u64> {
     matches
-        .value_of(k)?
-        .parse()
+        .try_get_one::<u64>(k)
         .ok()
+        .flatten()
+        .copied()
         .or_else(|| cfg.get_int(k).ok().map(|x| x as u64))
 }
 
 impl Settings {
-    pub fn new(matches: &clap::ArgMatches) -> Settings {
-        Settings {
-            debug: matches.is_present("debug"),
-            noudp: matches.is_present("noudp"),
-            path: matches
-                .value_of("config")
-                .unwrap_or("config.yml")
-                .to_owned(),
-            log: if matches.is_present("nolog") {
-                Log {
-                    enable_console: false,
-                    enable_file: false,
-                    file_directory: String::default(),
-                    level_filter: LevelFilter::Off,
-                }
-            } else {
-                Log {
-                    level_filter: if matches.is_present("debug") {
-                        LevelFilter::Debug
-                    } else {
-                        LevelFilter::Info
-                    },
-                    ..Default::default()
-                }
-            },
-            ..Default::default()
-        }
-    }
-    pub fn read_config(&self) -> Result<Config, config::ConfigError> {
-        let path = Path::new(&self.path);
+    fn read_config(config_path: &str) -> Config {
+        //  使用path读取配置文件
+        let path = Path::new(config_path);
         if !path.is_file() && std::fs::write(path, DEFAULT_CONFIG_FILE).is_err() {
-            error!("Can't create default config file 'config.yml', use default config and command line args.");
+            error!("Can't create default config file 'config.yml', use default config.");
         }
         Config::builder()
-            .add_source(config::File::new(&self.path, FileFormat::Yaml).required(false))
+            .add_source(config::File::new(config_path, FileFormat::Yaml).required(false))
             .build()
+            .expect("Can't read config file.")
     }
-    pub fn done(&mut self, matches: &clap::ArgMatches, cfg: &Config) {
-        if let Some(s) = get_str(matches, cfg, "mac") {
-            self.mac = Some(MacAddr::from_str(&s).expect("Can't parse MAC address!"));
+    fn resolve(mut settings: Settings, matches: &ArgMatches, cfg: Config) -> Settings {
+        // 解析配置文件
+        if let Some(s) = get_str(matches, &cfg, "mac") {
+            settings.mac = Some(MacAddr::from_str(&s).expect("Can't parse MAC address!"));
         }
 
-        if let Some(s) = get_str(matches, cfg, "ip") {
-            self.ip = Some(IpAddr::from_str(&s).expect("Can't parse IP Address!"));
+        if let Some(s) = get_str(matches, &cfg, "ip") {
+            settings.ip = Some(IpAddr::from_str(&s).expect("Can't parse IP Address!"));
         }
 
-        self.username = get_str(matches, cfg, "username").expect("Username is REQUIRED!");
-        self.password = get_str(matches, cfg, "password").expect("Password is REQUIRED!");
+        settings.username = get_str(matches, &cfg, "username").expect("Username is REQUIRED!");
+        settings.password = get_str(matches, &cfg, "password").expect("Password is REQUIRED!");
 
-        if let Some(mut s) = matches.value_of("dns").map(|s| s.to_string()) {
+        if let Some(mut s) = matches.try_get_one::<String>("dns").ok().flatten().cloned() {
             if (s.contains(']') && !s.contains("]:")) || !s.contains(':') {
                 s += ":53";
             }
-            self.dns
+            settings
+                .dns
                 .push(SocketAddr::from_str(&s).expect("Can't parse DNS server to socket address!"));
         }
         if let Ok(vs) = cfg.get_array("dns") {
@@ -261,75 +263,75 @@ impl Settings {
                 }
                 let x =
                     SocketAddr::from_str(&s).expect("Can't parse DNS server to socket address!");
-                if !self.dns.contains(&x) {
-                    self.dns.push(x);
+                if !settings.dns.contains(&x) {
+                    settings.dns.push(x);
                 }
             }
         }
 
-        if let Some(s) = get_str(matches, cfg, "host") {
-            self.host = s;
+        if let Some(s) = get_str(matches, &cfg, "host") {
+            settings.host = s;
         }
 
-        self.hostname = get_str(matches, cfg, "hostname").unwrap_or_else(|| {
+        settings.hostname = get_str(matches, &cfg, "hostname").unwrap_or_else(|| {
             hostname::get()
                 .expect("Can't get current computer host name.")
                 .into_string()
                 .expect("Can't parse host name to String.")
         });
 
-        if let Some(s) = get_str(matches, cfg, "time") {
-            self.time = NaiveTime::parse_from_str(&s, "%H:%M")
+        if let Some(s) = get_str(matches, &cfg, "time") {
+            settings.time = NaiveTime::parse_from_str(&s, "%H:%M")
                 .expect("Can't parse time String to NativeTime.");
         }
 
-        if let Some(x) = get_u64(matches, cfg, "reconnect") {
-            self.reconnect = x;
+        if let Some(x) = get_u64(matches, &cfg, "reconnect") {
+            settings.reconnect = x;
         }
 
         if let Ok(map) = cfg.get_table("heartbeat") {
             if let Some(x) = get_int_from_map(&map, "eap_timeout") {
-                self.heartbeat.eap_timeout = x as i32;
+                settings.heartbeat.eap_timeout = x as i32;
             }
             if let Some(x) = get_int_from_map(&map, "udp_timeout") {
-                self.heartbeat.udp_timeout = x as i32;
+                settings.heartbeat.udp_timeout = x as i32;
             }
         }
 
         if let Ok(map) = cfg.get_table("retry") {
             if let Some(x) = get_int_from_map(&map, "count") {
-                self.retry.count = max(x as i32, self.retry.count);
+                settings.retry.count = x as i32;
             }
             if let Some(x) = get_int_from_map(&map, "interval") {
-                self.retry.interval = max(x as i32, self.retry.interval);
+                settings.retry.interval = x as i32;
             }
         }
 
-        if self.log.level_filter != LevelFilter::Off {
+        #[cfg(feature = "log4rs")]
+        if settings.log.level_filter != log::LevelFilter::Off {
             if let Ok(map) = cfg.get_table("log") {
                 if let Some(x) = get_bool_from_map(&map, "enable_console") {
-                    self.log.enable_console = x;
+                    settings.log.enable_console = x;
                 }
                 if let Some(x) = get_bool_from_map(&map, "enable_file") {
-                    self.log.enable_file = x;
+                    settings.log.enable_file = x;
                 }
                 if let Some(x) = get_str_from_map(&map, "file_directory") {
-                    self.log.file_directory = x;
+                    settings.log.file_directory = x;
                 }
-                if self.debug {
-                    if let Some(Ok(level_filter)) = get_str_from_map(&map, "level")
-                        .map(|x| LevelFilter::from_str(x.to_ascii_uppercase().as_str()))
-                    {
-                        self.log.level_filter = level_filter;
-                    }
+                if let Some(Ok(level_filter)) = get_str_from_map(&map, "level")
+                    .map(|x| log::LevelFilter::from_str(x.to_ascii_uppercase().as_str()))
+                {
+                    settings.log.level_filter = level_filter;
                 }
             }
         }
+
         if let Ok(data) = cfg.get_table("data") {
             if let Some(map) = get_map_from_map(&data, "response_identity") {
                 if let Some(s) = get_str_from_map(&map, "unknown") {
                     if let Ok(v) = hex::decode(s) {
-                        self.data.response_identity.unknown = v;
+                        settings.data.response_identity.unknown = v;
                     } else {
                         error!("Invalid config: data.response_identity.unknown! Default value instead.")
                     }
@@ -338,7 +340,7 @@ impl Settings {
             if let Some(map) = get_map_from_map(&data, "response_md5_challenge") {
                 if let Some(s) = get_str_from_map(&map, "unknown") {
                     if let Ok(v) = hex::decode(s) {
-                        self.data.response_md5_challenge.unknown = v;
+                        settings.data.response_md5_challenge.unknown = v;
                     } else {
                         error!("Invalid config: data.response_md5_challenge.unknown! Default value instead.")
                     }
@@ -347,66 +349,71 @@ impl Settings {
             if let Some(map) = get_map_from_map(&data, "misc_info") {
                 if let Some(s) = get_str_from_map(&map, "unknown1") {
                     if let Ok(v) = hex::decode(s) {
-                        self.data.misc_info.unknown1 = v;
+                        settings.data.misc_info.unknown1 = v;
                     } else {
                         error!("Invalid config: data.misc_info.unknown1! Default value instead.")
                     }
                 }
                 if let Some(s) = get_str_from_map(&map, "cks32_param") {
                     if let Ok(v) = hex::decode(s) {
-                        self.data.misc_info.cks32_param = v;
+                        settings.data.misc_info.cks32_param = v;
                     } else {
                         error!("Invalid config: data.misc_info.cks32_param! Default value instead.")
                     }
                 }
                 if let Some(s) = get_str_from_map(&map, "unknown2") {
                     if let Ok(v) = hex::decode(s) {
-                        self.data.misc_info.unknown2 = v;
+                        settings.data.misc_info.unknown2 = v;
                     } else {
                         error!("Invalid config: data.misc_info.unknown2! Default value instead.")
                     }
                 }
                 if let Some(s) = get_str_from_map(&map, "os_major") {
                     if let Ok(v) = hex::decode(s) {
-                        self.data.misc_info.os_major = v;
+                        settings.data.misc_info.os_major = v;
                     } else {
                         error!("Invalid config: data.misc_info.os_major! Default value instead.")
                     }
                 }
                 if let Some(s) = get_str_from_map(&map, "os_minor") {
                     if let Ok(v) = hex::decode(s) {
-                        self.data.misc_info.os_minor = v;
+                        settings.data.misc_info.os_minor = v;
                     } else {
                         error!("Invalid config: data.misc_info.os_minor! Default value instead.")
                     }
                 }
                 if let Some(s) = get_str_from_map(&map, "os_build") {
                     if let Ok(v) = hex::decode(s) {
-                        self.data.misc_info.os_build = v;
+                        settings.data.misc_info.os_build = v;
                     } else {
                         error!("Invalid config: data.misc_info.os_build! Default value instead.")
                     }
                 }
                 if let Some(s) = get_str_from_map(&map, "os_unknown") {
                     if let Ok(v) = hex::decode(s) {
-                        self.data.misc_info.os_unknown = v;
+                        settings.data.misc_info.os_unknown = v;
                     } else {
                         error!("Invalid config: data.misc_info.os_unknown! Default value instead.")
                     }
                 }
                 if let Some(s) = get_str_from_map(&map, "version") {
                     if let Ok(v) = hex::decode(s) {
-                        self.data.misc_info.version = v;
+                        settings.data.misc_info.version = v;
                     } else {
                         error!("Invalid config: data.misc_info.version! Default value instead.")
                     }
                 }
                 if let Some(s) = get_str_from_map(&map, "hash") {
-                    self.data.misc_info.hash = s;
+                    settings.data.misc_info.hash = s;
                 }
             }
         }
+        settings
+    }
+    pub fn parse(matches: &ArgMatches) -> Settings {
+        let settings = Settings::default();
+        let path = matches.get_one::<String>("config").unwrap().to_owned();
+        let cfg = Settings::read_config(&path);
+        Settings::resolve(settings, matches, cfg)
     }
 }
-
-const DEFAULT_CONFIG_FILE: &str = include_str!("default_config.yml");
